9734a5b5868edbdc5ae522fca9c478d75730923d
MideAyo <frrezz001@gmail.com>
2023-03-16 15:23:47 +0100

    loyalbaze startup

---
+++ loyalbaze/src/components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar";
+import Features from "./Features";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import Access from "./Access";
+
+export { Navbar, Features, Hero, Footer, Access };
